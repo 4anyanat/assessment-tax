923e8ccb75f01f89ec21281b504ccf15672cf469
4anyanat <kanyanat.mnp@gmail.com>
2024-04-28 15:40:39 +0000

    Update graceful shutdown

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/4anyanat/assessment-tax
 go 1.22.2
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/labstack/echo/v4 v4.12.0
+	github.com/labstack/gommon v0.4.2
 	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.8.4
 )
@@ -12,7 +12,6 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"net/http"
+	"time"
+	
 	"github.com/4anyanat/assessment-tax/routes"
 	"github.com/4anyanat/assessment-tax/database"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Signal interface {
+	String() string
+	Signal()
+}
 
 func main() {
 	e := echo.New()
@@ -19,7 +28,25 @@ func main() {
 	database.DatabaseInit()
 
 	// Get port number from environment variable PORT
-	port := os.Getenv("PORT")
+	portNo := os.Getenv("PORT")
+	port := ":" + portNo
 
-	e.Logger.Fatal(e.Start(port))
+	go func() {
+		e.Logger.Fatal(e.Start(port))
+		if err := e.Start(port); err != nil && err != http.ErrServerClosed { 
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+
+	signal.Notify(shutdown, os.Interrupt)
+
+	<-shutdown
+	e.Logger.Fatal("Shutting down . . .")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
